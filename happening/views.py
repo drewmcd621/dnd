99505267671e23cabd858b929033317799d1b3a7
Drew McDermott <code@drewmcdermott.net>
2016-07-01 00:41:21 -0400

    Fix context

--- happening/views.py
+++ happening/views.py
@@ -6,5 +6,5 @@ from models import Game
 # Create your views here.
 def game(request, game):
     thegame = get_object_or_404(Game, url=game)
-    context = {'game', thegame }
+    context = {'game': thegame }
     return render(request, "main.html" , context)
