858f0e63cd8efb45b30b09ea955b67dd925531e0
Drew McDermott <code@drewmcdermott.net>
2016-06-30 23:47:09 -0400

    Make the model friendlier

--- happening/models.py
+++ happening/models.py
@@ -15,8 +15,11 @@ class Game(models.Model):
         (6, 'Sunday'),
     )
 
-    name = models.CharField(max_length=100)
+    name = models.CharField("Game name",max_length=100)
     host = models.ForeignKey(User, on_delete=models.CASCADE)
-    dow  = models.IntegerField(choices=DAYS_OF_WEEK, null=True)
-    time = models.TimeField(null=True)
-    happening = models.BooleanField(default=True)
+    dow  = models.IntegerField("Day of Week",choices=DAYS_OF_WEEK, null=True)
+    time = models.TimeField("Time",null=True)
+    happening = models.BooleanField("Is it happening this week?", default=True)
+
+    def __str__(self):
+        return self.name
