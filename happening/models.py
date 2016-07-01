d7f0644ed169627b1359a3b61946702bc356e1ee
Drew McDermott <code@drewmcdermott.net>
2016-07-01 11:11:21 -0400

    DOW display

--- happening/models.py
+++ happening/models.py
@@ -6,13 +6,13 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Game(models.Model):
     DAYS_OF_WEEK = (
-        (0, 'Monday'),
-        (1, 'Tuesday'),
-        (2, 'Wednesday'),
-        (3, 'Thursday'),
-        (4, 'Friday'),
-        (5, 'Saturday'),
-        (6, 'Sunday'),
+        (0, 'Sunday'),
+        (1, 'Monday'),
+        (2, 'Tuesday'),
+        (3, 'Wednesday'),
+        (4, 'Thursday'),
+        (5, 'Friday'),
+        (6, 'Saturday'),    
     )
 
     YES_NO = (
