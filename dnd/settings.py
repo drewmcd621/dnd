c2a86d2c261d80bf4ad0dd511a81481e71ea445a
Drew McDermott <code@drewmcdermott.net>
2016-07-01 11:58:44 -0400

    prep for prod

--- dnd/settings.py
+++ dnd/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wjhiuayz72la_%-f*z0g8+0-#ckt6-z23r-qjm8h77sbg7$98g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
